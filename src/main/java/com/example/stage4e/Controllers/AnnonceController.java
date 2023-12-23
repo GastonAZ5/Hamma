d212c147a0fcf4b894c5d9de92a9b288d02bed9f
GastonAZ5 <ghassen.bahry@esprit.tn>
2023-12-23 16:31:52 +0100

    Last commit

--- src/main/java/com/example/stage4e/Controllers/AnnonceController.java
+++ src/main/java/com/example/stage4e/Controllers/AnnonceController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/annonce")
 public class AnnonceController {
@@ -18,4 +20,21 @@ public class AnnonceController {
     public ResponseEntity<?> addPublication(@RequestBody Annonce annonce , @PathVariable Integer IdUser) {
         return new ResponseEntity<>(annonceService.addAnnonce(IdUser, annonce), HttpStatus.valueOf(200));
     }
+
+    @PutMapping("/updateAnnonce/{idAnnonce}/{idUser}")
+    public ResponseEntity<?> updateAnnonce(@RequestBody Annonce Annonce,@PathVariable Integer idAnnonce,@PathVariable Integer idUser) {
+        return new ResponseEntity<>(annonceService.updateAnnonce(Annonce,idAnnonce,idUser), HttpStatus.valueOf(200));
+    }
+
+    @DeleteMapping("/delete/{idAnnonce}")
+    public ResponseEntity<?> deleteAnnonce(@PathVariable Integer idAnnonce){
+        return new ResponseEntity<>(annonceService.deleteAnnonce(idAnnonce),HttpStatus.valueOf(200));
+    }
+
+
+    @GetMapping("/getAllAnnonce")
+    public List<Annonce> getAllPosts() {
+        return annonceService.getAllAnnonce();
+    }
+
 }
--- src/main/java/com/example/stage4e/Service/AnnonceServiceImpl.java
+++ src/main/java/com/example/stage4e/Service/AnnonceServiceImpl.java
@@ -8,6 +8,8 @@ import com.example.stage4e.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AnnonceServiceImpl implements AnnonceServiceInterface {
 
@@ -29,4 +31,37 @@ public class AnnonceServiceImpl implements AnnonceServiceInterface {
             return "Annonce ajouté avec succée";
         }
     }
+
+
+    public String deleteAnnonce(Integer idAnnonce){
+        annonceRepository.deleteById(idAnnonce);
+        return ("Annonce deleted succefully ");
+    }
+
+
+
+
+
+
+    public String updateAnnonce(Annonce annonce,Integer idAnnonce,Integer idUser) {
+
+
+
+        Annonce p = annonceRepository.findById(idAnnonce).get();
+        annonce.setIdAnnonce(p.getIdAnnonce());
+        User user = userRepository.findById(idUser).get();
+        annonce.setPublierPar(user);
+
+        BadWordServiceImp badWordServiceImp = new BadWordServiceImp();
+        if (badWordServiceImp.filterText(annonce.getContenu()).equals("This post contain bad word"))
+            return "This post contains bad word";
+        else {
+            annonceRepository.saveAndFlush(annonce);
+            return "Annonce updated succesfsuly";
+        }
+
+    }
+
+
+    public List<Annonce> getAllAnnonce(){return annonceRepository.findAll();}
 }
